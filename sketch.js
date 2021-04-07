2d2e567b5a5ab93c9e03ee791fc944b69e6b0eef
Prakrit-170807 <prak170807@gmail.com>
2021-04-07 23:43:46 +0530

    a clock

---
+++ sketch.js
@@ -0,0 +1,66 @@
+var H
+var M
+var S
+var G
+var B,I
+
+function preload(){
+  I= loadImage("logo(2).png")
+
+}
+
+function setup() {
+  createCanvas(windowWidth,windowHeight)
+  B=createSprite(240,height/2,20,20)
+  B.addImage(I)
+  B.scale=2
+}
+
+function draw() {
+  background(0);
+  H = hour();
+  M = minute();
+  S = second();
+  if(H==13){
+    H=H-12
+  }
+  if(H==14){
+    H=H-12
+  }
+  if(H==15){
+    H=H-12
+  }
+  if(H==16){
+    H=H-12
+  }
+  if(H==17){
+    H=H-12
+  }
+  if(H==18){
+    H=H-12
+  }
+  if(H==19){
+    H=H-12
+  }
+  if(H==20){
+    H=H-12
+  }
+  if(H==21){
+    H=H-12
+  }
+  if(H==22){
+    H=H-12
+  }
+  if(H==23){
+    H=H-12
+  }
+  if(H==0){
+    H=12
+  }
+  console.log(G);
+  textSize(280)
+  fill("white")
+  textFont("Mercy")
+  text(H+"⁢⁢⁢:"+M,width/2-180,height/2+80)
+  drawSprites();
+}
